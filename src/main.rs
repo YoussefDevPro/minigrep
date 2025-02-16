b7549b005a9f942eb0b611d9a96cb29354c3b611
YoussefPro-698 <youssef.pro.coding@gmail.com>
2025-02-16 16:05:24 +0100

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs;
+use std::io::prelude::*;
+
+fn truncate_content(contents: &str, max_length: usize) -> String {
+    if contents.len() > max_length {
+        let truncated = &contents[..max_length.saturating_sub(3)];
+        format!("{}...", truncated)
+    } else {
+        contents.to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let argues: Vec<String> = env::args().collect();
+    let query = &argues[1];
+    let filename = &argues[2];
+
+    println!("Searching for : {}", query);
+    let mut f = fs::File::open(filename)
+        .expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong when reading the file");
+    contents = truncate_content(&contents, 240);
+    println!("With text:\n{}", contents);
 }
