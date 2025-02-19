31d16ddb049fcd2229332b80c0b9897aa444620d
YoussefPro-698 <youssef.pro.coding@gmail.com>
2025-02-19 13:25:53 +0100

    'not enough arguments' error handling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use std::env;
 use std::fs;
 use std::io::prelude::*;
 
+fn main() {
+    let argues: Vec<String> = env::args().collect();
+    let (query, filename) = parse_config(&argues);
+    if query == "" && filename == "" {
+        println!("not enough arguments");
+    } else {
+        println!("Searching for : {}", query);
+        let mut f = fs::File::open(filename)
+            .expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong when reading the file");
+        contents = truncate_content(&contents, 240);
+        println!("With text:\n{}", contents);
+    }
+}
+
 fn truncate_content(contents: &str, max_length: usize) -> String {
     if contents.len() > max_length {
         let truncated = &contents[..max_length.saturating_sub(3)];
@@ -11,17 +28,12 @@ fn truncate_content(contents: &str, max_length: usize) -> String {
     }
 }
 
-fn main() {
-    let argues: Vec<String> = env::args().collect();
-    let query = &argues[1];
-    let filename = &argues[2];
-
-    println!("Searching for : {}", query);
-    let mut f = fs::File::open(filename)
-        .expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong when reading the file");
-    contents = truncate_content(&contents, 240);
-    println!("With text:\n{}", contents);
+fn parse_config(args: &[String]) -> (&str, &str) {
+    if args.len() < 3 {
+        ("", "")
+    } else {
+        let query = &args[1];
+        let filename = &args[2];
+        (query, filename)
+    }
 }
